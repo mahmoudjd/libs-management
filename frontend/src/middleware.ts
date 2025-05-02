59dd61e6183cd477bad560dab76bedbb8b334b1b
mahmoudjd <mahmoudjd452@gmail.com>
2025-05-02 19:02:11 +0200

    Improve the middleware

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,27 +1,26 @@
 import {NextRequest, NextResponse} from 'next/server';
 import { getToken } from 'next-auth/jwt';
 
-const protectedRoutes = ['/dashboard', '/loans' ,'/users'];
-const unprotectedRoutes = ['/login', '/register', '/books'];
+const protectedRoutes = new Set(['/dashboard', '/loans', '/users']);
 
 export async function middleware(req: NextRequest) {
-    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+    const {pathname} = req.nextUrl;
 
-    if (token && !protectedRoutes.includes(req.nextUrl.pathname) && !unprotectedRoutes.includes(req.nextUrl.pathname)) {
-        const url = req.nextUrl.clone();
-        url.pathname = '/dashboard';
-        return NextResponse.redirect(url);
+    const token = await getToken({req, secret: process.env.NEXTAUTH_SECRET});
+    const isLoggedIn = !!token;
+
+    if (pathname === '/') {
+        const redirectPath = isLoggedIn ? '/dashboard' : '/books';
+        return NextResponse.redirect(new URL(redirectPath, req.url));
     }
 
-    if (!token && protectedRoutes.includes(req.nextUrl.pathname)) {
-        const url = req.nextUrl.clone();
-        url.pathname = '/login';
-        return NextResponse.redirect(url);
+    if (!isLoggedIn && protectedRoutes.has(pathname)) {
+        return NextResponse.redirect(new URL('/login', req.url));
     }
 
     return NextResponse.next();
 }
 
 export const config = {
-    matcher: ['/dashboard', '/loans', '/users'],
+    matcher: ['/', '/dashboard', '/loans', '/users'],
 };
