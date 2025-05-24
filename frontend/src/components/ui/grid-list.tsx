dcbbd5f7e5b007a2a5db03d537e679e4b365f08b
mahmoudjd <mahmoudjd452@gmail.com>
2025-05-24 13:40:26 +0200

    feat: Add GridList component and update .gitignore for Vercel

---
+++ frontend/src/components/ui/grid-list.tsx
@@ -0,0 +1,9 @@
+interface GridListProps {
+    children: React.ReactNode;
+}
+
+export function GridList({children}: GridListProps) {
+    return (<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {children}
+    </div>)
+}
